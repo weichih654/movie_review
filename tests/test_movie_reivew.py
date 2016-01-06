301245ac72e7ddbcbea517b65d8104deababa7f8
Wade Liu <weichih654@gmail.com>
2016-01-06 23:18:00 +0800

    Make executable

--- tests/test_movie_reivew.py
+++ tests/test_movie_reivew.py
@@ -1,3 +1,10 @@
+import sys 
+import os
+sys.path.append(os.path.abspath("../"))
+from libs.movie_review import MovieReview
+from libs.review_searcher import ReviewSeacher
+from libs.matcher import Matcher
+
 if __name__ == "__main__":
     url = "https://www.ptt.cc/bbs/movie/M.1438186905.A.0EF.html"
     review = MovieReview (url)
