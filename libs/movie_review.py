79ae29b55515fa5d25ce2d3112b09c7856639e14
Wade Liu <weichih654@gmail.com>
2016-01-06 23:55:01 +0800

    Implement ptt parser

--- libs/movie_review.py
+++ libs/movie_review.py
@@ -1,3 +1,10 @@
+import urllib2
+import re
+
+def remove_string (pattern, content):
+    m = re.sub (pattern, "", content)
+    return m
+
 class MovieReview:
     def __init__ (self, link):
         self.link = link
@@ -18,10 +25,15 @@ class ReviewParser:
 class PttReviewParser(ReviewParser):
     def __init__ (self, link):
         self.link = link
+        response = urllib2.urlopen(link)
+        self.html = response.read()
 
     @property
     def content (self):
-        return ""
+        m = re.search('.*article-meta-value.*?<\/div\>(.*)\xe2\x80\xbb \xe7\x99\xbc\xe4\xbf\xa1\xe7\xab\x99', str(self.html), re.DOTALL)
+        cont = remove_string ("<.*?>", m.group(1))
+        cont = remove_string ("http[\w\/:\.-]*", cont)
+        return cont
 
 class EyesOpenReviewParser(ReviewParser):
     def __init__ (self, link):
