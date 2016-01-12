39148c02618c707cd619fb6551b394c0ff12bb93
Wade Liu <weichih654@gmail.com>
2016-01-12 10:38:12 +0800

    Add test case

--- tests/test_movie_reivew.py
+++ tests/test_movie_reivew.py
@@ -6,6 +6,12 @@ from libs.review_searcher import ReviewSeacher
 from libs.matcher import Matcher
 
 if __name__ == "__main__":
+    url = "http://www.moviemovie.com.tw/forum/viewthread-980.html"
+    review = MovieReview (url)
+    print "link = " + url
+    #print "type = " + review.type
+    print "content = " + review.content
+
     url = "https://www.ptt.cc/bbs/movie/M.1438186905.A.0EF.html"
     review = MovieReview (url)
     print "link = " + url
