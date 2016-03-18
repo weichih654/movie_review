39d01bc528838da10cc10087a890ae23e6b41e4e
Wade Liu <weichih654@gmail.com>
2016-03-19 02:27:00 +0800

    check folder

--- tests/test_match.py
+++ tests/test_match.py
@@ -6,6 +6,8 @@ sys.path.append(os.path.abspath("../"))
 from libs.movie_review import MovieReview
 from libs.review_searcher import ReviewSeacher
 from libs.matcher import Matcher
+import os
+from os import path
 
 def readfile(filename): 
     f = open(filename, "r")
@@ -18,22 +20,27 @@ def readfile(filename):
     f.close()
     return content
 
-if __name__ == "__main__":
-    input_file = sys.argv[1]
-    content = readfile (input_file)
-    print "check file " + input_file
-    movie_name = "新世紀福爾摩斯"
+allurl = []
+reviews = {}
+def match (filename, movie_name):
     searcher = ReviewSeacher (movie_name)
-    allurl = searcher.reviews
+    content = readfile (filename)
+    global allurl
+    global reviews
+    if len(allurl) == 0:
+        allurl = searcher.reviews
     i = 0
+    print "file = " + filename
     for p in allurl:
-        review = MovieReview (p)
-        #print "link = " + p
-        #print "type = " + review.type
-        #print "content = " + review.content
+        review = None
+        if p not in reviews:
+            review = MovieReview (p)
+            reviews[p] = review
+        else:
+            review = reviews[p]
         matcher = Matcher()
         ratio = matcher.match (content, review.content)
-        if ratio > 40:
+        if ratio > 35:
             print "ratio = " + str(ratio) + " %"
             print "link = " + p
         i = i + 1
@@ -41,4 +48,13 @@ if __name__ == "__main__":
         sys.stdout.write("\r%d %%" % progress)
         sys.stdout.flush()
     print "\n"    
+
+if __name__ == "__main__":
+    movie_name = sys.argv[1]
+    input_path = sys.argv[2]
+    print "path " + input_path
+    files = [f for f in os.listdir(input_path)]
+
+    for f in files:
+        match (input_path + "/" + f, movie_name)
 
